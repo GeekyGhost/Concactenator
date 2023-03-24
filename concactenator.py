06fcde931b64c1b74331c3335cc74b31b8143d8d
GeekyGhost <111990299+GeekyGhost@users.noreply.github.com>
2023-03-24 00:13:57 -0400

    Update concactenator.py
    added audio feature for video creation.

--- concactenator.py
+++ concactenator.py
@@ -3,6 +3,7 @@ import cv2
 import tkinter as tk
 from tkinter import filedialog
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
+from moviepy.editor import AudioFileClip
 
 # Custom colors and fonts
 bg_color = "#1a1a1a"
@@ -26,6 +27,11 @@ def get_next_project_folder(base_output_folder):
             break
         i += 1
     return project_folder
+
+def browse_audio_file():
+    selected_audio_file = filedialog.askopenfilename(filetypes=[("Audio files", "*.mp3;*.wav;*.ogg")])
+    if selected_audio_file:
+        audio_path.set(selected_audio_file)
 
 def select_video_file():
     video_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4;*.avi;*.mkv;*.mov;*.flv;*.wmv")])
@@ -69,7 +75,13 @@ def convert_images_to_video(folder_path, output_file, fps):
         print(f"No image files found in {folder_path}")
         return
 
-    video = ImageSequenceClip(image_paths, fps=fps)
+    audio_file = audio_path.get()
+    if audio_file and os.path.exists(audio_file):
+        audio_clip = AudioFileClip(audio_file)
+        video = ImageSequenceClip(image_paths, fps=fps).set_audio(audio_clip)
+    else:
+        video = ImageSequenceClip(image_paths, fps=fps)
+
     video.write_videofile(output_file)
 
     print(f"Video saved to {os.path.abspath(output_file)}")
@@ -103,9 +115,10 @@ folder_path = tk.StringVar()
 output_file = tk.StringVar()
 fps = tk.StringVar(value="15")
 video_path = tk.StringVar()
+audio_path = tk.StringVar(value="")
 
 # Frame for input folder and output file selection
-selection_frame = tk.LabelFrame(root, text="Select Input and Output", padx=5, pady=5, bg=bg_color, fg=text_color)
+selection_frame = tk.LabelFrame(root, text="Select Image, Video, and/or Audio Files", padx=5, pady=5, bg=bg_color, fg=text_color)
 selection_frame.grid(row=0, column=0, padx=10, pady=10)
 
 tk.Label(selection_frame, text="Images:").grid(row=0, column=0)
@@ -115,6 +128,10 @@ tk.Button(selection_frame, text="Browse", command=browse_image_folder).grid(row=
 tk.Label(selection_frame, text="Video:").grid(row=1, column=0)
 tk.Entry(selection_frame, textvariable=video_path).grid(row=1, column=1)
 tk.Button(selection_frame, text="Browse", command=browse_video_file).grid(row=1, column=2)
+
+tk.Label(selection_frame, text="Audio:").grid(row=2, column=0)
+tk.Entry(selection_frame, textvariable=audio_path).grid(row=2, column=1)
+tk.Button(selection_frame, text="Browse", command=browse_audio_file).grid(row=2, column=2)
 
 # Frame for FPS and action buttons
 action_frame = tk.LabelFrame(root, text="Settings and Actions", padx=5, pady=5, bg=bg_color, fg=text_color)
