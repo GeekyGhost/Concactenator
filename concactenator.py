da3e1fabab3d92058dc9d10e24bf7e93f10f3d62
GeekyGhost <111990299+GeekyGhost@users.noreply.github.com>
2023-03-24 17:24:34 -0400

    Update concactenator.py
    Made some changes to UI color, altered the script so that the FPS of the images is determined by the tempo and the length of the audio file used so that the image transitions and the music both last the same length of time.

--- concactenator.py
+++ concactenator.py
@@ -1,13 +1,17 @@
 import os
 import cv2
 import tkinter as tk
+import numpy as np
 from tkinter import filedialog
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
+from moviepy.editor import concatenate_videoclips, ImageClip
 from moviepy.editor import AudioFileClip
+from aubio import source, tempo
+
 
 # Custom colors and fonts
-bg_color = "#1a1a1a"
-text_color = "#00ff00"
+bg_color = "#1e1e1e"
+text_color = "#9eff9e"
 button_color = "#333333"
 button_hover_color = "#4c4c4c"
 font = "Helvetica 10 bold"
@@ -32,6 +36,31 @@ def browse_audio_file():
     selected_audio_file = filedialog.askopenfilename(filetypes=[("Audio files", "*.mp3;*.wav;*.ogg")])
     if selected_audio_file:
         audio_path.set(selected_audio_file)
+
+def extract_beats(audio_file):
+    win_s = 512                 # fft size
+    hop_s = win_s // 2          # hop size
+
+    samplerate = 0
+    s = source(audio_file, samplerate, hop_s)
+    samplerate = s.samplerate
+    o = tempo("default", win_s, hop_s, samplerate)
+
+    beats = []
+
+    # total number of frames read
+    total_frames = 0
+    while True:
+        samples, read = s()
+        is_beat = o(samples)
+        if is_beat:
+            this_beat = o.get_last_s()
+            beats.append(this_beat)
+        total_frames += read
+        if read < hop_s:
+            break
+
+    return np.array(beats)
 
 def select_video_file():
     video_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4;*.avi;*.mkv;*.mov;*.flv;*.wmv")])
@@ -110,7 +139,34 @@ def start_conversion():
     project_folder = get_next_project_folder("output")
     output_file_name = f"concatenated_video.mp4"
     output_file_with_path = os.path.join(project_folder, output_file_name)
-    convert_images_to_video(folder_path.get(), output_file_with_path, int(fps.get()))
+
+    audio_file = audio_path.get()
+    audio_duration = None
+
+    if audio_file and os.path.exists(audio_file):
+        audio_clip = AudioFileClip(audio_file)
+        audio_duration = audio_clip.duration
+
+    if audio_duration is not None:
+        fps_value = int(fps.get())
+        create_video_with_audio_duration(folder_path.get(), output_file_with_path, fps_value, audio_duration, audio_clip)
+    else:
+        convert_images_to_video(folder_path.get(), output_file_with_path, int(fps.get()))
+
+def create_video_with_audio_duration(folder_path, output_file, fps, audio_duration, audio_clip):
+    image_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.jpg') or f.endswith('.png')]
+
+    if not image_paths:
+        print(f"No image files found in {folder_path}")
+        return
+
+    image_duration = audio_duration / len(image_paths)
+    image_clips = [ImageClip(p).set_duration(image_duration).resize(height=480) for p in image_paths]
+
+    video = concatenate_videoclips(image_clips, method="compose").set_audio(audio_clip)
+    video.write_videofile(output_file, fps=fps)
+
+    print(f"Video saved to {os.path.abspath(output_file)}")
 
 root = tk.Tk()
 root.title("Image/Video Processing")
